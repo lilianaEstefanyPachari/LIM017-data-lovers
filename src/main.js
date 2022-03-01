4e43dc0d3c9e450887b46fbd727318366513c1b3
lilianaEstefanyPachari <lilianapachari21@gmail.com>
2022-02-28 21:24:32 -0500

    se hicieron cambios en css para armar la esctructura con css grid, tambien en main.js realizamos codigo para traer la data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,49 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js';
 
-console.log(example, data);
+/* const cajita = document.createElement("img");
+cajita.src = data.films[0].poster;
+document.getElementById("cover").appendChild(cajita);
+
+
+let peliculas = data.films;
+const movieName=document.querySelector(".name");
+//const movieYear=document.querySelector(".year");
+for (let i of peliculas) {
+    movieName.innerHTML+="<br> Name" + i.title +"<br> Year: "+i.release_date ;
+    //movieYear.innerHTML+=i.release_date;
+console.log (i.title)
+console.log(i.release_date)
+} */
+
+
+
+
+
+console.log(example, data); //example es una funcion y data un objeto
+console.log(typeof data);
+console.log(data);
+
+
+
+
+
+
+
+
+
+// let peliculas = data.films;
+
+// for (let i of peliculas) {
+//     // console.log (i.title)
+//     // console.log(i.release_date)
+//     // // console.log(i.people)
+//     let personas = i.people;
+//     for (let propiedad of personas){
+//         console.log(propiedad.name);
+//     }
+// };
+
