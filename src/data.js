2466e9ba6fcb83006880b5a692e4f3b53046bea2
lilianaEstefanyPachari <lilianapachari21@gmail.com>
2022-03-07 18:35:36 -0500

    crear filtros en el html y enlazar las funciones filter y sort

--- src/data.js
+++ src/data.js
@@ -1,8 +1,38 @@
-// estas funciones son de ejemplo
+// funcion busc
+console.log("holaaaaaaa");
 
-export const example = () => {
-  return 'example';
+export const filterData = (data,condition) => {
+  let filterMovie = data.filter(searchFunction);
+  function searchFunction(item) {
+    if (item.title==condition) {
+      return true;
+    }
+    return false;
+  }
+  return filterMovie;
 };
+
+//console.log(filterData(movies2,"Castle in the Sky"));
+
+//copia de seguridad
+export const sortData = (data, sortBy, sortOrder) => {
+  if (sortOrder=="OrdAsc") {
+    return data.sort(function orderAsc(a,b){
+      if(a.sortBy > b.sortBy) { return -1; }
+      if(a.sortBy < b.sortBy) { return 1; }
+      return 0;
+    });
+  } else if (sortOrder=="OrdDesc"){
+    return data.sort(function orderDesc(a,b){
+      if(a.sortBy < b.sortBy) { return -1; }
+      if(a.sortBy > b.sortBy) { return 1; }
+      return 0;
+    });
+  }
+};
+
+//console.log(sortData(movies2,title,"OrdAsc"));
+
 
 export const anotherExample = () => {
   return 'OMG';
--- src/main.js
+++ src/main.js
@@ -1,17 +1,25 @@
-import { example } from "./data.js";
+//import { example } from "./data.js";
 // import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 import data from "./data/ghibli/ghibli.js";
+import { filterData, sortData } from "./data.js";
 
+console.table(sortData(data.films, "release_date", "OrdDesc"));
+console.table(sortData(data.films, "release_date", "OrdAsc"));
 //number of movies
 const numberOfMovies = data.films.length;
 document.getElementById("numberOfMovies").innerHTML =
   numberOfMovies + " PELICULAS";
 
-let movies2 = data.films;
+//barra de busqueda
+const searchTextValue = document.getElementById("searchMovie").value;
+
+
+console.log(filterData(data.films, searchTextValue));
 
 //data
+let movies2 = data.films;
 for (let i of movies2) {
   const divImgMovie = document.createElement("div");
   const groupMovie = document.createElement("div");
@@ -33,6 +41,134 @@ for (let i of movies2) {
 }
 
 //obtener datos del input text searchMovie y obtener la coincidencia
+const searchBtn = document.getElementById("btnSearchMovie");
+searchBtn.addEventListener("click", searching);
+function searching() {
+  document.getElementById("Movies").style.display = "block";
+  document.getElementById("listOfMovies").style.display = "none";
+  document.getElementById("numberOfMovies").style.display = "block";
+
+  const searchTextValue = document.getElementById("searchMovie").value;
+ 
+  const filterResult = filterData(data.films, searchTextValue);
+  console.log(typeof filterResult);
+
+  for (let i of filterResult) {
+    const groupMovie = document.createElement("div");
+    const divImgMovie = document.createElement("div");
+    const imgMovie = document.createElement("img");
+    const nameMovie = document.createElement("p");
+    const yearMovie = document.createElement("p");
+    imgMovie.src = i.poster;
+    nameMovie.innerText = i.title;
+    yearMovie.innerText = i.release_date;
+
+    groupMovie.setAttribute("class", "group2");
+    divImgMovie.setAttribute("class", "cover");
+    nameMovie.setAttribute("class", "name");
+    yearMovie.setAttribute("class", "year");
+    document.querySelector(".theMovie").appendChild(groupMovie);
+    groupMovie.appendChild(divImgMovie);
+    divImgMovie.appendChild(imgMovie);
+    divImgMovie.appendChild(nameMovie);
+    divImgMovie.appendChild(yearMovie);
+  }
+}
+
+//obtener filtros de año
+const yearBoton= document.getElementById("selectYear");
+console.log (yearBoton);
+yearBoton.addEventListener("click", filterFunction);
+function filterFunction() {
+  document.getElementById("Movies").style.display = "block";
+  document.getElementById("listOfMovies").style.display = "none";
+  document.getElementById("numberOfMovies").style.display = "block";
+  let yearSelect= document.getElementById("selectYear").value;
+  console.log (yearSelect);
+  let filterResultYear = sortData(data.films, "release_date", "OrdDesc");
+  console.table (filterResultYear);
+  for (let i of filterResultYear) {
+    const groupMovie = document.createElement("div");
+    const divImgMovie = document.createElement("div");
+    const imgMovie = document.createElement("img");
+    const nameMovie = document.createElement("p");
+    const yearMovie = document.createElement("p");
+    imgMovie.src = i.poster;
+    nameMovie.innerText = i.title;
+    yearMovie.innerText = i.release_date;
+
+    groupMovie.setAttribute("class", "group2");
+    divImgMovie.setAttribute("class", "cover");
+    nameMovie.setAttribute("class", "name");
+    yearMovie.setAttribute("class", "year");
+    document.querySelector(".theMovie").appendChild(groupMovie);
+    groupMovie.appendChild(divImgMovie);
+    divImgMovie.appendChild(imgMovie);
+    divImgMovie.appendChild(nameMovie);
+    divImgMovie.appendChild(yearMovie);
+  }
+}
+
+
+//Orden por año de mayor a menor
+
+// let s = movies2.sort(function (a, b) {
+//   if (a.release_date > b.release_date) {
+//     return -1;
+//   }
+//   if (a.release_date < b.release_date) {
+//     return 1;
+//   }
+//   return 0;
+// });
+// console.table(s);
+
+console.table(sortData(data.films, "release_date", "OrdAsc"));
+
+//Orden por año de menor a mayor
+let h = movies2.sort(function (a, b) {
+  if (a.release_date < b.release_date) {
+    return -1;
+  }
+  if (a.release_date > b.release_date) {
+    return 1;
+  }
+  return 0;
+});
+console.table(h);
+console.table(sortData(data.films, "release_date", "OrdDesc"));
+
+// //Orden por
+/*let y = movies2.sort(function (a, b) {
+  if (a.title < b.title) {
+    return -1;
+  }
+  if (a.title > b.title) {
+    return 1;
+  }
+  return 0;
+});*/
+//console.table(y);
+
+//sortData(movies2,orderAsc,);
+
+//Para sacar la información de toda la información segun la busqueda
+// const x = movies2.filter(predicate);
+// function predicate(item) {
+//   console.log(item.title);
+//   if (item.title == "My Neighbor Totoro") {
+//     return item.title;
+//   }
+//   return false;
+// }
+// console.table(x);
+
+
+
+
+/*
+//obtener datos del input text searchMovie y obtener la coincidencia
+
 const searchBtn = document.getElementById("btnSearchMovie");
 searchBtn.addEventListener("click", searching);
 function searching() {
@@ -74,30 +210,4 @@ document.getElementById("numberOfMovies").style.display = "none";
   }
 
 }
-
-//Orden por año de mayor a menor
-let s= movies2.sort(function(a, b){
-  if(a.release_date > b.release_date) { return -1; }
-  if(a.release_date < b.release_date) { return 1; }
-  return 0;
-})
-console.table(s);
-
-//Orden por 
-let y= movies2.sort(function(a, b){
-  if(a.title < b.title) { return -1; }
-  if(a.title > b.title) { return 1; }
-  return 0;
-})
-console.table(y);
-
-//Para sacar la información de toda la información segun la busqueda
-// const x = movies2.filter(predicate);
-// function predicate(item) {
-//   console.log(item.title);
-//   if (item.title == "My Neighbor Totoro") {
-//     return item.title;
-//   }
-//   return false;
-// }
-// console.table(x);
+*/
