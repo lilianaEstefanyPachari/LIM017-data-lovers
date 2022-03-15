b67b89b26fda587d2e876b4f16dedbb4e44c6260
Karina Macedo <ruth.macedo@ucsp.edu.pe>
2022-03-14 20:30:28 -0600

    data estadistica, top 10

--- src/data.js
+++ src/data.js
@@ -1,44 +1,122 @@
 import data from "./data/ghibli/ghibli.js";
 
-export const searchMovieInfo = (data,condition) => {
+export const searchMovieInfo = (data, condition) => {
   let filterMovieInfo = data.filter(searchFunction);
   function searchFunction(item) {
-    if (item.title==condition)
-    { return true;
+    if (item.title == condition) {
+      return true;
     } else {
-      return false;}
+      return false;
+    }
   }
   return filterMovieInfo;
 };
 
 //console.log(searchMovieInfo(data.films,"Castle in the Sky"));
 
-export const filterData = (data,condition) => {
+export const filterData = (data, condition) => {
   let filterMovie = data.filter(searchFunction);
   function searchFunction(item) {
-    let titleMovie=item.title.toLowerCase();
-    if (titleMovie.indexOf(condition)!==-1) {
+    let titleMovie = item.title.toLowerCase();
+    if (titleMovie.indexOf(condition) !== -1) {
       return true;
     }
     return false;
   }
   return filterMovie;
 };
-console.log(filterData(data.films,"Castle in the Sky"));
+console.log(filterData(data.films, "Castle in the Sky"));
 
-export const sortData = (data,sortBy,sortOrder) => {
-
-  if (sortOrder=="OrdAsc") {
-    return data.sort(function (a,b) {
-      if(a[sortBy] < b[sortBy]) {return -1; }
-      if(a[sortBy] > b[sortBy]) {return 1; }
+export const sortData = (data, sortBy, sortOrder) => {
+  // if (sortBy === "rt_score"){
+  if (sortOrder == "OrdAsc" && sortBy === "rt_score") {
+    return data.sort(function (a, b) {
+      if (parseInt(a[sortBy]) < parseInt(b[sortBy])) {
+        return -1;
+      }
+      if (parseInt(a[sortBy]) > parseInt(b[sortBy])) {
+        return 1;
+      }
       return 0;
     });
-  } else if (sortOrder=="OrdDesc"){
-    return data.sort(function (a,b) {
-      if(a[sortBy] > b[sortBy]) {return -1; }
-      if(a[sortBy] < b[sortBy]) {return 1; }
+  }
+  if (sortOrder == "OrdDesc" && sortBy === "rt_score") {
+    return data.sort(function (a, b) {
+      if (parseInt(a[sortBy]) > parseInt(b[sortBy])) {
+        return -1;
+      }
+      if (parseInt(a[sortBy]) < parseInt(b[sortBy])) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+  // } else {
+
+  if (sortOrder == "OrdAsc" && sortBy !== "rt_score") {
+    return data.sort(function (a, b) {
+      if (a[sortBy] < b[sortBy]) {
+        return -1;
+      }
+      if (a[sortBy] > b[sortBy]) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+  if (sortOrder == "OrdDesc" && sortBy !== "rt_score") {
+    return data.sort(function (a, b) {
+      if (a[sortBy] > b[sortBy]) {
+        return -1;
+      }
+      if (a[sortBy] < b[sortBy]) {
+        return 1;
+      }
       return 0;
     });
   }
 };
+// }
+
+export const computeStats = (data, sortBy) => {
+  let topValues = data.sort((a, b) => b[sortBy] - a[sortBy]).slice(0, 10);
+  return topValues;
+};
+
+// obtener cuantos directores producieron la pelicula
+const movieData = data.films;
+const countsDirector = {};
+movieData.forEach((x) => {
+  countsDirector[x.director] = (countsDirector[x.director] || 0) + 1;
+});
+console.log(countsDirector);
+
+const arr = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "a",
+  "b",
+  "c",
+  "f",
+  "g",
+  "h",
+  "h",
+  "h",
+  "e",
+  "a",
+];
+const counts = {};
+arr.forEach((x) => {
+  counts[x] = (counts[x] || 0) + 1;
+});
+console.log(arr.length);
+console.log(counts);
+console.log(typeof counts);
+
+//  var sema = ["lu","ma","mi"];
+//  for (i in sema) {
+//  console.log(sema[i]);
+//  }
--- src/main.js
+++ src/main.js
@@ -3,110 +3,130 @@
 //import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 import data from "./data/ghibli/ghibli.js";
-import { searchMovieInfo, filterData, sortData } from "./data.js";
+import { searchMovieInfo, filterData, sortData, computeStats } from "./data.js";
 
 // console.table(sortData(data.films, "release_date", "OrdDesc"));
 // console.table(sortData(data.films, "release_date", "OrdAsc"));
 
 //el numero de las peliculas
-function numberMovies(numberMovie){
-const numberOfMovies = numberMovie.length;
-document.getElementById("numberOfMovies").innerHTML = numberOfMovies + " Movies";
+function numberMovies(numberMovie) {
+  const numberOfMovies = numberMovie.length;
+  document.getElementById("numberOfMovies").innerHTML =
+    numberOfMovies + " Movies";
 }
 
 //mostrar data con HTML
-const listOfMovies=document.querySelector(".listOfMovies");
-const themovie=document.querySelector(".theMovie");
-const infoContainer=document.querySelector('.infoContainer');
-const people=document.querySelector('.people');
-const locations=document.querySelector('.locations');
-const vehicles=document.querySelector('.vehicles');
-let movies2=data.films;
-function showData(movies2,place){
-  listOfMovies.innerHTML="";
-  themovie.innerHTML="";
-  infoContainer.innerHTML="";
-  const persons=movies2[0].people;
-  const locationsMovie=movies2[0].locations;
-  const vehiclesMovie=movies2[0].vehicles;
-  if (place==infoContainer){
-    for (let i of movies2) {
-      place.innerHTML+= `
-      
+const listOfMovies = document.querySelector(".listOfMovies");
+const themovie = document.querySelector(".theMovie");
+const infoContainer = document.querySelector(".infoContainer");
+const infoData = document.querySelector(".infoData");
+const people = document.querySelector(".people");
+const locations = document.querySelector(".locations");
+const vehicles = document.querySelector(".vehicles");
+
+let movies2 = data.films;
+function showData(movies2, place) {
+  listOfMovies.innerHTML = "";
+  themovie.innerHTML = "";
+  infoContainer.innerHTML = "";
+  infoData.innerHTML = "";
+  const persons = movies2[0].people;
+  const locationsMovie = movies2[0].locations;
+  const vehiclesMovie = movies2[0].vehicles;
+  switch (place) {
+    case infoContainer:
+      for (let i of movies2) {
+        place.innerHTML += `
+              <div class="movieImg">
+                <img src=${i.poster}>
+              </div>
+              <div class="infoMovie">
+                <h3 class="titleMovie">${i.title}</h3>
+                <p class="subtitles"> Description </p>
+                <p class="parag">${i.description}</p>
+                <p class="subtitles">Director </p>
+                <p class="parag">${i.director}</p>
+                <p class="subtitles">Producer</p>
+                <p class="parag">${i.producer}</p>
+                <p class="subtitles">Release Date</p>
+                <p class="parag">${i.release_date}</p>
+                <div class="scoreMovie">
+                   <p class="subtitles">Score: </p>
+                   <p class="parag">${i.rt_score}</p>
+                </div>
+              </div>`;
+      }
+      for (let i of persons) {
+        people.innerHTML += `
+              <div class="groupInfoItems">
+                <div class="coverInfoItems">
+                  <img src=${i.img}>
+                </div>
+                <p class="nameInfoPeople">${i.name}</p>
+              </div> `;
+      }
+      for (let i of locationsMovie) {
+        locations.innerHTML += `
+              <div class="groupInfoLocation">
+                <div class="coverLocation" style="background-image: url(${i.img})">
+                </div>
+                <div class="nameOfLocation">
+                 <p class="nameItems">${i.name}</p>
+                </div>
+              </div> `;
+      }
+      for (let i of vehiclesMovie) {
+        vehicles.innerHTML += `
+              <div class="groupInfoVehicles">
+                 <div class="coverVehicles" style="background-image: url(${i.img})">
+                 </div>
+                 <div class="nameOfVehicles">
+                    <p class="nameItems">${i.name}</p>
+                 </div>
+              </div> `;
+      }
+      break;
+    case infoData:
+      for (let i of movies2) {
+        place.innerHTML += `
         <div class="movieImg">
-          <img src=${i.poster}> 
-        </div>
-        <div class="infoMovie">
-          <h3 class="titleMovie">${i.title}</h3>
-          <p class="subtitles"> Description </p> 
-          <p class="parag">${i.description}</p> 
-          <p class="subtitles">Director </p> 
-          <p class="parag">${i.director}</p> 
-          <p class="subtitles">Producer</p> 
-          <p class="parag">${i.producer}</p> 
-          <p class="subtitles">Release Date</p> 
-          <p class="parag">${i.release_date}</p> 
-          <div class="scoreMovie">
-             <p class="subtitles">Score: </p>
-             <p class="parag">${i.rt_score}</p> 
-          </div>
-        </div>`;
-    }
-    for (let i of persons) {
-      people.innerHTML+= `
-        <div class="groupInfoItems">
-          <div class="coverInfoItems">
-            <img src=${i.img}>
-            
-          </div>
-          <p class="nameInfoPeople">${i.name}</p>
-        </div> `;
-    }
-    for (let i of locationsMovie) {
-      locations.innerHTML+= `
-        <div class="groupInfoLocation">
-          <div class="coverLocation" style="background-image: url(${i.img})">
-          </div>
-          <div class="nameOfLocation">
-           <p class="nameItems">${i.name}</p>
-          </div>
-        </div> `;
-    }
-    for (let i of vehiclesMovie) {
-      vehicles.innerHTML+= `
-        <div class="groupInfoVehicles">
-           <div class="coverVehicles" style="background-image: url(${i.img})">
-           </div>
-           <div class="nameOfVehicles">
-              <p class="nameItems">${i.name}</p>
-           </div>           
-        </div> `;
-    }
-    
-  } else {
-    for (let i of movies2) {
-      place.innerHTML+= `
-        <div class="group2">
-        <div class="cover">
         <img src=${i.poster}>
-        <p class="name">${i.title}</p>
-        <p class="year">${i.release_date}</p>
-        <p class="score"> Score: ${i.rt_score}</p>
-        </div>
-        </div> `;
-    }
+      </div>
+      <div class="infoMovie">
+        <h3 class="titleMovie">${i.title}</h3>
+        <p class="subtitles"> Description </p>
+        <p class="parag">${i.description}</p>`;
+      }
+      break;
+    default:
+      for (let i of movies2) {
+        place.innerHTML += `
+              <div class="group2">
+              <div class="cover">
+              <img src=${i.poster}>
+              <p class="name">${i.title}</p>
+              <p class="year">${i.release_date}</p>
+              <p class="score"> Score: ${i.rt_score}</p>
+              </div>
+              </div> `;
+      }
   }
-  
-  if (place.innerHTML===""){
-    place.innerHTML+= `
+  if (place.innerHTML === "") {
+    place.innerHTML += `
     <h1> No se encontro la búsqueda </h1>`;
   }
 }
 numberMovies(movies2);
-showData(movies2,themovie);
+showData(movies2, themovie);
+clickCoverAll();
 
-
-
+//monstrar informacion de cada pelicula
+function clickCoverAll() {
+  const divCover = document.querySelectorAll(".cover");
+  divCover.forEach((addDivCover) => {
+    addDivCover.addEventListener("click", clickCover);
+  });
+}
 
 /*
 //jalar  la data general a la interfaz con appendChild
@@ -138,12 +158,15 @@ function searching() {
   document.getElementById("Movies").style.display = "block";
   document.getElementById("listOfMovies").style.display = "none";
   document.getElementById("numberOfMovies").style.display = "block";
-  let searchTextValue = document.getElementById("searchMovie").value.toLowerCase();
+  let searchTextValue = document
+    .getElementById("searchMovie")
+    .value.toLowerCase();
   //let searchTextValue2 = searchTextValue.replace(/ /g, "");
   const filterResult = filterData(data.films, searchTextValue);
   console.log(filterResult);
   numberMovies(filterResult);
-  showData(filterResult,themovie);
+  showData(filterResult, themovie);
+  clickCoverAll();
 
   /*for (let i of filterResult) {
     themovie.innerHTML+= `
@@ -178,21 +201,23 @@ function searching() {
 }
 
 //obtener filtros de AÑO
-const yearBoton= document.getElementById("selectYear");
+const yearBoton = document.getElementById("selectYear");
 yearBoton.addEventListener("click", filterFunction);
 
 function filterFunction() {
-  if (yearBoton.value == "Year"){return}
+  if (yearBoton.value === "Year") {
+    return;
+  }
   document.getElementById("Movies").style.display = "block";
   document.getElementById("listOfMovies").style.display = "none";
   document.getElementById("numberOfMovies").style.display = "block";
-  let yearSelect= document.getElementById("selectYear").value;
-  console.log (yearSelect);
+  let yearSelect = document.getElementById("selectYear").value;
+  console.log(yearSelect);
 
-  let filterResultYear = sortData(data.films, "release_date" , yearSelect);
+  let filterResultYear = sortData(data.films, "release_date", yearSelect);
   numberMovies(filterResultYear);
-  showData(filterResultYear,themovie);
-
+  showData(filterResultYear, themovie);
+  clickCoverAll();
   /*for (let i of filterResultYear) {
     const groupMovie = document.createElement("div");
     const divImgMovie = document.createElement("div");
@@ -214,21 +239,23 @@ function filterFunction() {
   }*/
 }
 
-
 //obtener filtro de A-Z
 
-const botonAZ= document.getElementById("selectOrder");
+const botonAZ = document.getElementById("selectOrder");
 botonAZ.addEventListener("click", filterFunction2);
 
 function filterFunction2() {
-  if( botonAZ.value == "A-Z"){return}
+  if (botonAZ.value === "A-Z") {
+    return;
+  }
   document.getElementById("Movies").style.display = "block";
   document.getElementById("listOfMovies").style.display = "none";
   document.getElementById("numberOfMovies").style.display = "block";
-  let azSelect= document.getElementById("selectOrder").value;
-  let filterResultTitle = sortData(data.films, "title" , azSelect);
+  let azSelect = document.getElementById("selectOrder").value;
+  let filterResultTitle = sortData(data.films, "title", azSelect);
   numberMovies(filterResultTitle);
-  showData(filterResultTitle,themovie);
+  showData(filterResultTitle, themovie);
+  clickCoverAll();
   /*
   for (let i of filterResultTitle) {
     const groupMovie = document.createElement("div");
@@ -253,53 +280,50 @@ function filterFunction2() {
 
 //filtro por score
 
-const botonScore= document.getElementById("selectScore");
+const botonScore = document.getElementById("selectScore");
 botonScore.addEventListener("click", filterFunction3);
 
 function filterFunction3() {
-  if( botonScore.value == "Score"){return}
+  if (botonScore.value === "Score") {
+    return;
+  }
   document.getElementById("Movies").style.display = "block";
   document.getElementById("listOfMovies").style.display = "none";
   document.getElementById("numberOfMovies").style.display = "block";
-  let scoreSelect= document.getElementById("selectScore").value;
-  let filterResultScore = sortData(data.films, "rt_score" , scoreSelect);
+  let scoreSelect = document.getElementById("selectScore").value;
+  let filterResultScore = sortData(data.films, "rt_score", scoreSelect);
   numberMovies(filterResultScore);
-  showData(filterResultScore,themovie);
+  showData(filterResultScore, themovie);
+  clickCoverAll();
 }
-
-
-
 
 //funcion no filtrar
 
-const botonDontFilter= document.getElementById("noFilter");
+const botonDontFilter = document.getElementById("noFilter");
 botonDontFilter.addEventListener("click", dontfilterFunction);
 
 function dontfilterFunction() {
   // $("#selectScore").load(" #selectScore > *");
   //$( "#listOfMovies" ).load(window.location.href + " #listOfMovies" );
-  const selectBtn = document.getElementById("selectOrder");
-  selectBtn.options[0].selected="selected"
+  const selectOrder = document.getElementById("selectOrder");
+  const selectYear = document.getElementById("selectYear");
+  const selectScore = document.getElementById("selectScore");
+  selectOrder.options[0].selected = "selected";
+  selectYear.options[0].selected = "selected";
+  selectScore.options[0].selected = "selected";
+
+  //problem
+  numberMovies(movies2);
+  showData(movies2, themovie);
 }
-
-
-
-
-
-
 
 // yearBoton.value == "noFilter";
 //   numberMovies(movies2);
 //   showData(movies2,listOfMovies);
 
-
 //filtro por score
-console.table(sortData(data.films,"rt_score","OrdAsc"));
-console.table(sortData(data.films,"rt_score","OrdDesc"));
-
-
-
-
+console.table(sortData(data.films, "rt_score", "OrdAsc"));
+console.table(sortData(data.films, "rt_score", "OrdDesc"));
 
 //Orden por año de mayor a menor
 
@@ -313,10 +337,6 @@ console.table(sortData(data.films,"rt_score","OrdDesc"));
 //   return 0;
 // });
 // console.table(s);
-
-
-
-
 
 /*
 
@@ -337,8 +357,6 @@ console.table(sortData(data.films, "release_date", "OrdDesc"));
 
 
 */
-
-
 
 // //Orden por
 /*let y = movies2.sort(function (a, b) {
@@ -364,9 +382,6 @@ console.table(sortData(data.films, "release_date", "OrdDesc"));
 //   return false;
 // }
 // console.table(x);
-
-
-
 
 /*
 //obtener datos del input text searchMovie y obtener la coincidencia
@@ -413,16 +428,17 @@ document.getElementById("numberOfMovies").style.display = "none";
 
 }
 */
-const divCover=document.querySelectorAll(".cover");
-divCover.forEach((addDivCover) => {
-  addDivCover.addEventListener("click", clickCover)
-});
 
+//funcion para el scroll top
+function scroll() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
 
 function clickCover(e) {
   document.querySelector(".homePage").style.display = "none";
   document.querySelector(".container").style.display = "none";
   document.querySelector(".moviePage").style.display = "block";
+  scroll();
   console.log("holaaaaa");
   obtainName(e.target);
   //const objeto={ name:selectCover.querySelector(".name").textContent;}
@@ -430,13 +446,13 @@ function clickCover(e) {
   //
 }
 
-function obtainName(objeto){
+function obtainName(objeto) {
   console.log(objeto);
-  const movieName=objeto.querySelector('.name').textContent;
+  const movieName = objeto.querySelector(".name").textContent;
   console.log(movieName);
-  const filterResult = searchMovieInfo(data.films,movieName);
+  const filterResult = searchMovieInfo(data.films, movieName);
   console.log(filterResult);
-  showData(filterResult,infoContainer);
+  showData(filterResult, infoContainer);
   // for (let i of filterResult) {
   //   infoMovie.innerHTML+= `
   //     <div class="group2">
@@ -448,9 +464,20 @@ function obtainName(objeto){
   //     </div>
   //     </div> `;
   // }
-  
   // if (movieName=="Castle in the Sky"){
   //   console.log("Yes");
   // } else{
   //   console.log("No");}
 }
+
+const topMovies = document.getElementById("topMovies");
+topMovies.addEventListener("click", dataTop);
+function dataTop() {
+  console.log("holaaasdasd");
+  document.querySelector(".container").style.display = "none";
+  document.querySelector(".dataStatics").style.display = "block";
+  const topData = computeStats(data.films, "rt_score");
+  showData(topData, infoData);
+}
+
+// console.table(dataTop());
